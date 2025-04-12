0865e4fde739709bb87cfe8093d25e496ef8e711
chelunike <chelunike@gmail.com>
2025-04-12 09:42:16 +0200

    Random Commits Update

--- task.sh
+++ task.sh
@@ -1,16 +1,24 @@
 #! /bin/bash
 
 # VARS
-REPO_PATH="YOUR_REPO_PATH_HERE"
+REPO_PATH="REPO_PATH_HERE"
 
 cd $REPO_PATH
 
-# Do something
-echo $RANDOM > random_number.txt
+# Get Random number
+num=$((RANDOM % 3 + 1))
 
-# Proced to commit
-git add random_number.txt
-git commit -m "I do something :)"
+# Generate commits
+for ((i = 1; i <= num; i++)); do
+	# Do something
+	echo $RANDOM > random_number.txt
 
-# Never do this
-git push origin main
+	# Proced to commit
+	git add random_number.txt
+	git commit -m "I do something :)"
+
+	# Never do this
+	git push origin main
+done
+
+
